85dba0895ab4e3622650b4ee442f42f156f9e21f
toka <tokaabdulhamied@gmail.com>
2022-12-01 12:06:48 +0200

    create FollowersList tests without mocking the api request

---
+++ src/components/FollowersList/FollowersList.test.js
@@ -0,0 +1,28 @@
+import { BrowserRouter } from "react-router-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import FollowersList from "./FollowersList";
+
+/*FollowersList Test Stories 
+    - should render follower item
+    - should render multible followers items 
+
+*/
+const FollowersListMock = () => (
+  <BrowserRouter>
+    <FollowersList />
+  </BrowserRouter>
+);
+
+describe("FollowersList", () => {
+  it("should render follower item", async () => {
+    render(<FollowersListMock />);
+    const FollowerDivItem = await screen.findByTestId("follower-item-0");
+    expect(FollowerDivItem).toBeInTheDocument();
+  });
+
+  it("should render multible followers items", async () => {
+    render(<FollowersListMock />);
+    const FollowersDivItems = await screen.findAllByTestId(/follower-item/i);
+    expect(FollowersDivItems.length).toBe(5);
+  });
+});
